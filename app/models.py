f2c5322719e1df8662dde159bb2c75b8106bcacf
TONY CARLOS DE CARVALHO RE CUPELLO <tony.cupello@aluno.senac.rio>
2026-09-23 21:46:19 -0300

    'aula10'

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# tipo string -> CharField()
+# tipo inteiro -> IntegerField()
+# tipo decimal -> FloatField()
+# tipo booleano -> BooleanField()
+
+class Produtos(models.Model):
+    nome = models.CharField(max_length=200)
+    preco = models.FloatField()
+    estoque = models.IntegerField()
+    imagem = models.CharField(max_length=500, default='')
+
+    def __str__(self):
+        return self.nome
--- app/views.py
+++ app/views.py
@@ -1,17 +1,12 @@
 from django.shortcuts import render
-
+from .models import Produtos
 
 def home_view(request):
     return render(request,'home.html')
 
 def produtos_view(request):
 
-    lista_produtos = [{'nome': "Monitor", "preco": 700.00, "estoque": 3},
-               {'nome': "Mouse", "preco": 150.00, "estoque": 5},
-               {'nome': "Teclado", "preco": 190.00, "estoque": 7},
-               {'nome': "Mousepad", "preco": 200.00, "estoque": 2},
-               {'nome': "Headset", "preco": 300.00, "estoque": 9},
-               ]
+    lista_produtos = Produtos.objects.all()
     context = {'produtos' : lista_produtos}
     
     return render(request,'produtos.html', context)
